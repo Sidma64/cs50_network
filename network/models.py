45e3644fe3bae1cb60c0528ff93d8d3dbc8777f5
Sidma64 <sidma.ultra@gmail.com>
2022-05-10 19:30:28 +0300

    Create models for Post and Comments

--- network/models.py
+++ network/models.py
@@ -1,6 +1,20 @@
+from tkinter import CASCADE
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.CharField(max_length=2048)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name="likes")
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    body = models.CharField(max_length=1024)
+    date = models.DateField(auto_now=True)
